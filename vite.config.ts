98e9a0c1be97950d4c04ebbd23fd44c067acc332
Misa Liu <misaliu@misaliu.top>
2025-05-01 17:52:00 +0800

    chore(vite): Automatically generate index.d.ts by Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   build: {
     lib: {
       entry: './lib/main.ts',
-      name: 'Counter',
-      fileName: 'counter',
+      name: '@phifans/audio',
+      formats: [ 'es', 'umd' ],
+      fileName: 'phifans-audio',
     },
   },
+  plugins: [
+    dts({
+      rollupTypes: true,
+      include: [ './lib/**/*' ],
+      exclude: [ './src/**/*' ],
+    }),
+  ],
 })
