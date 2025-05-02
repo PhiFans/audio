a9f8af6b6c664e0d8bfab2e32d256483ee24625b
Misa Liu <misaliu@misaliu.top>
2025-05-02 17:09:08 +0800

    feat: Automatically resume audio context

--- lib/const.ts
+++ lib/const.ts
@@ -2,8 +2,23 @@
 
 import Ticker from './ticker';
 import Clock from './clock';
+import { resumeAudioCtx } from './utils';
 
 const AudioCtx = window.AudioContext || window.webkitAudioContext;
 export const GlobalAudioCtx = new AudioCtx({ latencyHint: 'interactive' });
 export const GlobalAudioTicker = new Ticker();
 export const GlobalAudioClock = new Clock(GlobalAudioCtx, GlobalAudioTicker);
+
+const _resumeAudioCtx = async () => {
+  const result = await resumeAudioCtx(GlobalAudioCtx);
+  if (!result) return;
+  if (GlobalAudioCtx.state !== 'running') return;
+
+  window.removeEventListener('pointerdown', _resumeAudioCtx);
+};
+
+window.addEventListener('load', () => {
+  if (GlobalAudioCtx.state === 'running') return;
+
+  window.addEventListener('pointerdown', _resumeAudioCtx);
+});
